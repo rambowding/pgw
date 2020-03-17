ab353a4893018f0fd8af215df776bf13c58ff813
dingxirong <dingxirong@baidu.com>
2020-03-17 23:10:37 +0800

    增加数学加减乘除的逻辑

--- lib/math_detail.dart
+++ lib/math_detail.dart
@@ -26,89 +26,173 @@ class _MathDetailState extends State<MathDetail> {
         providers: [
           ChangeNotifierProvider.value(value: ResultAreaModel()),
           ChangeNotifierProvider.value(value: ArrowAreaModel()),
+          ChangeNotifierProvider.value(value: OperatorModel()),
         ],
         child: SafeArea(
           child: Column(
             children: <Widget>[
+              // 算术符区
+              Card(
+                elevation: 4,
+                margin: EdgeInsets.fromLTRB(12, 12, 12, 0),
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Consumer2<OperatorModel, ResultAreaModel>(
+                    builder: (context, OperatorModel opModel, ResultAreaModel resultModel,  _) {
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          FloatingActionButton.extended(
+                            heroTag: '+',
+                            onPressed: () {
+                              opModel.update(0);
+                              resultModel.resetResult();
+                            },
+                            label: Text('加(+)', style: TextStyle(color: CupertinoColors.systemOrange),),
+                            backgroundColor: _updateOpAreaColor(opModel.value == 0),
+                          ),
+                          FloatingActionButton.extended(
+                            heroTag: '-',
+                            onPressed: () {
+                              opModel.update(1);
+                              resultModel.resetResult();
+                            },
+                            label: Text('减(-)', style: TextStyle(color: CupertinoColors.systemOrange),),
+                            backgroundColor: _updateOpAreaColor(opModel.value == 1),
+                          ),
+                          FloatingActionButton.extended(
+                            heroTag: '×',
+                            onPressed: () {
+                              opModel.update(2);
+                              resultModel.resetResult();
+                            },
+                            label: Text('乘(×)', style: TextStyle(color: CupertinoColors.systemOrange),),
+                            backgroundColor: _updateOpAreaColor(opModel.value == 2),
+                          ),
+                          FloatingActionButton.extended(
+                            heroTag: '÷',
+                            onPressed: () {
+                              opModel.update(3);
+                              resultModel.resetResult();
+                            },
+                            label: Text('除(÷)', style: TextStyle(color: CupertinoColors.systemOrange),),
+                            backgroundColor: _updateOpAreaColor(opModel.value == 3),
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ),
               // 结果区
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Expanded(
-                    child: Consumer2<ResultAreaModel, ArrowAreaModel>(
-                      builder: (context, ResultAreaModel resultModel, ArrowAreaModel arrowMode, _) {
-                        return GestureDetector(
-                          onTap: () => arrowMode.update(true),
-                          child: Container(
-                            margin: EdgeInsets.all(15),
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
+              Card(
+                elevation: 4,
+                margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Expanded(
+                      child: Consumer2<ResultAreaModel, ArrowAreaModel>(
+                        builder: (context, ResultAreaModel resultModel, ArrowAreaModel arrowMode, _) {
+                          return GestureDetector(
+                            onTap: () => arrowMode.update(true),
+                            child: Container(
+                              margin: EdgeInsets.all(15),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                              ),
+                              child: Text(
+                                resultModel.left,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
                             ),
-                            child: Text(
-                              resultModel.left,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    flex: 5,
-                  ),
-                  Expanded(
-                    child: Text('+',
-                      textAlign: TextAlign.center,
-                    ),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: Consumer2<ResultAreaModel, ArrowAreaModel>(
-                      builder: (context, ResultAreaModel resultModel, ArrowAreaModel arrowMode, _) {
-                        return GestureDetector(
-                          onTap: () => arrowMode.update(false),
-                          child: Container(
-                            margin: EdgeInsets.all(15),
-                            padding: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
-                              borderRadius: BorderRadius.all(Radius.circular(10)),
-                            ),
-                            child: Text(
-                              resultModel.right,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        );
-                      },
-                    ),
-                    flex: 5,
-                  ),
-                  Expanded(
-                    child: Text('=',
-                      textAlign: TextAlign.center,
-                    ),
-                    flex: 1,
-                  ),
-                  Expanded(
-                    child: Container(
-                      margin: EdgeInsets.all(15),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                          );
+                        },
                       ),
-                      child: Text('?',
+                      flex: 6,
+                    ),
+                    Expanded(
+                      child: Consumer<OperatorModel>(
+                        builder: (context, OperatorModel model, _) {
+                          return Text(_updateOperatorChar(model.value),
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 30),
+                          );
+                        },
+                      ),
+                      flex: 1,
+                    ),
+                    Expanded(
+                      child: Consumer2<ResultAreaModel, ArrowAreaModel>(
+                        builder: (context, ResultAreaModel resultModel, ArrowAreaModel arrowMode, _) {
+                          return GestureDetector(
+                            onTap: () => arrowMode.update(false),
+                            child: Container(
+                              margin: EdgeInsets.all(15),
+                              padding: EdgeInsets.all(10),
+                              decoration: BoxDecoration(
+                                border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
+                                borderRadius: BorderRadius.all(Radius.circular(10)),
+                              ),
+                              child: Text(
+                                resultModel.right,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                      flex: 6,
+                    ),
+                    Expanded(
+                      child: Text('=',
                         textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 30),
                       ),
+                      flex: 1,
                     ),
-                    flex: 5,
-                  ),
-                ],
+                    Expanded(
+                      child: Consumer2<ResultAreaModel, OperatorModel>(
+                          builder: (context, ResultAreaModel resultModel, OperatorModel opModel, _) {
+                            return GestureDetector(
+                              onTap: () => resultModel.updateResult(opModel.value),
+                              child: Container(
+                                margin: EdgeInsets.all(15),
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: CupertinoColors.systemGrey, width: 0.5),
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                ),
+                                child: Text(resultModel.result == -1 ? '?' : adjustResult(resultModel.result.toString()),
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
+                      ),
+                      flex: 6,
+                    ),
+                  ],
+                ),
               ),
               // 箭头区
               Container( // 此区域用Container来wrap一下，否则高度会过高，导致超过屏幕尺寸，原因未知
                 height: 80,
+                margin: EdgeInsets.fromLTRB(12, 0, 12, 0),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: <Widget>[
@@ -383,18 +467,41 @@ class _MathDetailState extends State<MathDetail> {
       ),
     );
   }
-}
 
-/// 测试局部刷新用，暂时保留
-class MyFab extends StatelessWidget {
-  final index;
+  /// 更新算术符区的背景色
+  Color _updateOpAreaColor(bool selected) {
+    return selected ? CupertinoColors.systemIndigo : CupertinoColors.white;
+  }
 
-  const MyFab(this.index);
+  /// 更新算术符
+  String _updateOperatorChar(int value) {
+    String char = '+';
+    switch (value) {
+      case 0:
+        char = '+';
+        break;
+      case 1:
+        char = '-';
+        break;
+      case 2:
+        char = '×';
+        break;
+      case 3:
+        char = '÷';
+        break;
+      default:
+        char = '+';
+        break;
+    }
+    return char;
+  }
 
-  @override
-  Widget build(BuildContext context) {
-    print('---------------MyTab build---index=$index');
-    return Icon(Icons.add);
+  /// 去掉结果中的.0字符
+  String adjustResult(String result) {
+    if (result.endsWith('.0')) {
+      return result.substring(0, result.length - 2);
+    }
+    return result;
   }
 }
 
@@ -403,6 +510,7 @@ class ResultAreaModel with ChangeNotifier {
 
   String left = '0';
   String right = '0';
+  double result = -1; // 默认值，表示未计算
 
   void update(String key, bool isSelectLeft) {
     if (isSelectLeft) {
@@ -428,8 +536,45 @@ class ResultAreaModel with ChangeNotifier {
     }
 //    print('-------isLeft=$isSelectLeft----------left=$left----------right=$right');
 
+    result = -1; // 需要重复结果，否则不会作计算操作，参见updateResult方法
     // 通知消费者更新
     notifyListeners();
+  }
+
+  void updateResult(int operator) {
+    // 当result为非-1时，表示已经经过计算，此时不再重复计算
+    if (result == -1) {
+      switch (operator) {
+        case 0:
+          result = double.parse(left) + double.parse(right);
+          break;
+        case 1:
+          result = double.parse(left) - double.parse(right);
+          break;
+        case 2:
+          result = double.parse(left) * double.parse(right);
+          break;
+        case 3:
+          double tmpRight = double.parse(right);
+          if (tmpRight == 0) {
+            BotToast.showText(text: '除数不能为0');
+            result = 0; // 表示经过计算了
+          } else {
+            result = double.parse(left) / tmpRight;
+          }
+          break;
+        default:
+          break;
+      }
+      notifyListeners();
+    }
+  }
+
+  void resetResult() {
+    if (result != -1) {
+      result = -1;
+      notifyListeners();
+    }
   }
 }
 
@@ -442,6 +587,20 @@ class ArrowAreaModel with ChangeNotifier {
     if (isLeft == left) return;
 
     isLeft = left;
+    notifyListeners();
+  }
+}
+
+/// 算术符model
+class OperatorModel with ChangeNotifier {
+
+  // 0表示加法，1表示减法，2表示乘法，3表示除法
+  int value = 0;
+
+  void update(int newValue) {
+    if (value == newValue) return;
+
+    value = newValue;
     notifyListeners();
   }
 }
