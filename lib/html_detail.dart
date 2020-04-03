d20b82e044f005fa12d8093a24b8a059e34006a4
dingxirong <dingxirong@baidu.com>
2020-04-03 14:29:08 +0800

    增加计算机HTML的界面

--- lib/html_detail.dart
+++ lib/html_detail.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:pgw/it_tab.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HtmlDetail extends StatefulWidget {
 
@@ -9,14 +11,40 @@ class HtmlDetail extends StatefulWidget {
 }
 
 class _HtmlDetailState extends State<HtmlDetail> {
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
+      resizeToAvoidBottomInset: false,
       navigationBar: CupertinoNavigationBar(
         previousPageTitle: ItTab.title,
+        trailing: CupertinoButton(
+          padding: EdgeInsets.zero,
+          child: Icon(CupertinoIcons.refresh_bold),
+        ),
       ),
-      child: Container(),
+      child: SafeArea(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+                child: CupertinoTextField(
+                  placeholder: '请在这输入html',
+                  maxLines: null,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: CupertinoColors.systemGrey, width: 2),
+                  ),
+                ),
+            ),
+            Expanded(
+              child: WebView(
+                initialUrl: 'https://www.baidu.com',
+              ),
+              flex: 1,
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
