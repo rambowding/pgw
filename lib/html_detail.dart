c70ef6e0f86c23947fae9cd48be428c7daeda48c
dingxirong <dingxirong@baidu.com>
2020-04-07 18:24:16 +0800

    通过webview加载输入框的内容

--- lib/html_detail.dart
+++ lib/html_detail.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pgw/it_tab.dart';
@@ -12,15 +15,28 @@ class HtmlDetail extends StatefulWidget {
 
 class _HtmlDetailState extends State<HtmlDetail> {
 
+  final Completer<WebViewController> _webviewController =
+      Completer<WebViewController>();
+
+  final _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       resizeToAvoidBottomInset: false,
       navigationBar: CupertinoNavigationBar(
         previousPageTitle: ItTab.title,
-        trailing: CupertinoButton(
-          padding: EdgeInsets.zero,
-          child: Icon(CupertinoIcons.refresh_bold),
+        trailing: FutureBuilder<WebViewController>(
+          future: _webviewController.future,
+          builder: (BuildContext context, AsyncSnapshot<WebViewController> controller){
+            return CupertinoButton(
+              padding: EdgeInsets.zero,
+              child: Icon(CupertinoIcons.refresh_bold),
+              onPressed: () {
+                _loadTextFieldContent(controller.data, _textEditingController.text);
+              },
+            );
+          },
         ),
       ),
       child: SafeArea(
@@ -29,6 +45,7 @@ class _HtmlDetailState extends State<HtmlDetail> {
             Expanded(
               flex: 1,
                 child: CupertinoTextField(
+                  controller: _textEditingController,
                   placeholder: '请在这输入html',
                   maxLines: null,
                   decoration: BoxDecoration(
@@ -36,15 +53,27 @@ class _HtmlDetailState extends State<HtmlDetail> {
                   ),
                 ),
             ),
+            Text('网页title'),
             Expanded(
               child: WebView(
                 initialUrl: 'https://www.baidu.com',
+                onWebViewCreated: (WebViewController controller) {
+                  _webviewController.complete(controller);
+                },
               ),
-              flex: 1,
+              flex: 2,
             ),
           ],
         ),
       ),
     );
+  }
+
+  /// 通过webview加载输入框的内容
+  void _loadTextFieldContent(WebViewController controller, String text) async{
+//    print('------------------------text=$text');
+    final String contentBase64 =
+      base64Encode(const Utf8Encoder().convert(text));
+    await controller.loadUrl('data:text/html;base64,$contentBase64');
   }
 }
